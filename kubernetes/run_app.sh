e02b64c2d4fc8c0b68812c5e031aea3d8b92e2c6
zhangmingming <582852387@qq.com>
2018-08-27 11:08:32 +0800

    修改执行脚本

--- kubernetes/run_app.sh
+++ kubernetes/run_app.sh
@@ -4,11 +4,11 @@ if [ $# -eq 2 ]; then
     app_name="$2"
     kubectl="kubectl -s $server"
     if [ "$($kubectl get rc -l name=${app_name}|grep ${app_name}|awk '{{ print $1 }}')" != "" ]; then
-        $kubectl delete -f smart-cityos-datav-center-rc.yml
+        $kubectl delete -f ${app_name}-rc.yml
     fi
     sleep 5
     if [ "$($kubectl get svc -l name=${app_name}|grep ${app_name}|awk '{{ print $1 }}')" = "" ]; then
-        $kubectl create -f smart-cityos-datav-center-svc.yml
+        $kubectl create -f ${app_name}-svc.yml
     fi
-    $kubectl create -f smart-cityos-datav-center-rc.yml
+    $kubectl create -f ${app_name}-rc.yml
 fi
