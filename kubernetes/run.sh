250ea92a1d859213666518f5892c0005b66b4ea2
zhangmingming <582852387@qq.com>
2018-08-27 10:40:53 +0800

    修改k8s配置

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -4,11 +4,11 @@ if [ $# -eq 2 ]; then
     app_name="$2"
     kubectl="kubectl -s $server"
     if [ "$($kubectl get rc -l name=${app_name}|grep ${app_name}|awk '{{ print $1 }}')" != "" ]; then
-        $kubectl delete -f app-rc.yml
+        $kubectl delete -f smart-cityos-datav-center-rc.yml
     fi
     sleep 5
     if [ "$($kubectl get svc -l name=${app_name}|grep ${app_name}|awk '{{ print $1 }}')" = "" ]; then
-        $kubectl create -f app-svc.yml
+        $kubectl create -f smart-cityos-datav-center-svc.yml
     fi
-    $kubectl create -f app-rc.yml
+    $kubectl create -f smart-cityos-datav-center-rc.yml
 fi
