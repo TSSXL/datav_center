79d15855f712507cdac0247eba8cf77a07271fd5
zhangmingming <582852387@qq.com>
2018-08-29 11:36:20 +0800

    修改执行脚本

--- kubernetes/run_app.sh
+++ kubernetes/run_app.sh
@@ -4,11 +4,13 @@ if [ $# -eq 2 ]; then
     app_name="$2"
     kubectl="kubectl -s $server"
     if [ "$($kubectl get rc -l name=${app_name}|grep ${app_name}|awk '{{ print $1 }}')" != "" ]; then
+        $kubectl delete -f ${app_name}-config.yml
         $kubectl delete -f ${app_name}-rc.yml
     fi
     sleep 5
     if [ "$($kubectl get svc -l name=${app_name}|grep ${app_name}|awk '{{ print $1 }}')" = "" ]; then
         $kubectl create -f ${app_name}-svc.yml
     fi
+    $kubectl create -f ${app_name}-config.yml
     $kubectl create -f ${app_name}-rc.yml
 fi
