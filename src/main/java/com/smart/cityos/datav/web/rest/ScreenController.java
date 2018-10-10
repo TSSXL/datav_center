4cf7375d68c9dfe0f0e71468a3421bb9258bbd12
方国锋 <beckfun@163.com>
2018-10-10 14:16:01 +0800

    修复前端分页组件不匹配的bug

--- src/main/java/com/smart/cityos/datav/web/rest/ScreenController.java
+++ src/main/java/com/smart/cityos/datav/web/rest/ScreenController.java
@@ -45,11 +45,11 @@ public class ScreenController {
 
   @PostMapping("/list/cover")
   @ApiOperation("可视化设计列表接口")
-  public Page<Screen> list(@RequestParam Long pageNo, @RequestParam Long pageSize,
+  public Page<Screen> list(@RequestParam Long currentPage, @RequestParam Long pageSize,
       @RequestParam String sort, @RequestParam Long order,@RequestBody List< ScreenQueryBody> screenQueryBody) {
     Sort sort1 = new Sort((order.equals(1) ? Direction.ASC : Direction.DESC),
         (sort.isEmpty() ? "_id" : sort));
-    Pageable pageable = new PageRequest(pageNo.intValue(), pageSize.intValue(), sort1);
+    Pageable pageable = new PageRequest((currentPage.intValue() - 1), pageSize.intValue(), sort1);
     return screenService
         .fetch(screenQueryBody.size() > 0 ? screenQueryBody.get(0) : null, pageable);
   }
